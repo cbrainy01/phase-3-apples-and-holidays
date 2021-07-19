db3be7e1f8329ee27c1b571ba370d0056e5d9a51
Carl <carlbrainegebe@gmail.com>
2021-07-19 12:48:47 -0500

    commit tree 7da20899e586bded70f50f5efe6d58e33e3b0b59

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,35 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -62,6 +66,31 @@ def all_supplies_in_holidays(holiday_hash)
   # - Convert the symbol to a string ("fourth_of_july")
   # - Replace each underscore with a space ("fourth of july")
   # - Capitalize the first letter of each word ("Fourth Of July")
+  seasons = holiday_hash.keys
+   count = 0
+#  binding.pry
+ 
+ while count < seasons.length
+  puts "#{seasons[count]}:"
+  subcount = 0
+  # holidayscount = seasons[count].keys.length
+
+  while subcount < seasons[count].keys.length do
+    # prints out the holiday for current season and in that holiday, 
+    # iterate through its array and print out its decorations 
+    puts"#{seasons[count][subcount]}"
+    decoration_count = 0
+    while decoration_count < seasons[count][subcount].length do
+      puts "#{seasons[count][subcount][decoration_count]}, "
+      decoration_count += 1;
+    end
+
+    subcount += 1
+  end 
+  binding.pry
+ end
+
+# puts holiday_hash
 end
 
 def all_holidays_with_bbq(holiday_hash)
